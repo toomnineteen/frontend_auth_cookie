247ba1b21e664339c06cd870916ab54e1587cc7c
toomnineteen <toomnineteen@gmail.com>
2025-11-27 14:31:53 +0700

    update

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,29 @@
 import axios from "axios";
 
-export const register = async (data) => { return axios.post(`${import.meta.env.VITE_BASE_URL}/api/auth/register`, data )}
-export const login = async (data) => { return axios.post(`${import.meta.env.VITE_BASE_URL}/api/auth/login`, data, { withCredentials: true })}
-export const logout = async () => { return axios.post(`${import.meta.env.VITE_BASE_URL}/api/auth/logout`, {}, { withCredentials: true })}
-export const profile = async () => {return axios.get(`${import.meta.env.VITE_BASE_URL}/api/auth/me`, { withCredentials: true })}
+export const register = async (data) => {
+  return axios.post(
+    `${import.meta.env.VITE_BASE_URL}/api/auth/register`,
+    data,
+    { withCredentials: true } // ✅ ต้องมี!
+  );
+};
+
+export const login = async (data) => {
+  return axios.post(`${import.meta.env.VITE_BASE_URL}/api/auth/login`, data, {
+    withCredentials: true,
+  });
+};
+
+export const logout = async () => {
+  return axios.post(
+    `${import.meta.env.VITE_BASE_URL}/api/auth/logout`,
+    {},
+    { withCredentials: true }
+  );
+};
+
+export const profile = async () => {
+  return axios.get(`${import.meta.env.VITE_BASE_URL}/api/auth/me`, {
+    withCredentials: true,
+  });
+};
